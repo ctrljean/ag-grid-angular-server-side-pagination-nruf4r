959e361fe0b12a205d7fe53fd627f582c3e11138
ctrljean <38362323+ctrljean@users.noreply.github.com>
2023-03-19 01:53:24 -0500

    Work in pagination changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,32 @@
 import { Component } from '@angular/core';
-import { GridApi } from 'ag-grid-community';
+import { GridApi, PaginationChangedEvent } from 'ag-grid-community';
+import { map, tap } from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { ResponseBackend, Datum } from './backend';
 import { RemoteGridApi } from './remote-grid-api';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements RemoteGridApi {
-  columnDefs = [{ field: 'make' }, { field: 'model' }, { field: 'price' }];
-
-  rowData = [
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 }
+  columnDefs = [
+    { field: 'id' },
+    { field: 'first_name' },
+    { field: 'last_name' },
+    { field: 'avatar' },
+    { field: 'email' },
   ];
+
+  rowData = [];
 
   gridOptions = {
     pagination: true,
     rowModelType: 'infinite',
-    cacheBlockSize: 17,
-    paginationPageSize: 17
+    cacheBlockSize: 6,
+    paginationPageSize: 6,
   };
 
   gridApi: GridApi;
@@ -46,6 +37,25 @@ export class AppComponent implements RemoteGridApi {
   }
 
   getData(params): Observable<{ data; totalRecords }> {
-    return of({ data: this.rowData, totalRecords: 85 });
+    const url = `https://reqres.in/api/users`;
+    let perPage: number;
+    let totalPages: number;
+    let total: number;
+
+    return ajax.getJSON<ResponseBackend>(url).pipe(
+      map<ResponseBackend, any>((response) => {
+        perPage = response.per_page;
+        totalPages = response.total_pages;
+        total = response.total;
+        return { data: response.data, totalRecords: response.total };
+      })
+    );
+  }
+
+  changedPagination(event: PaginationChangedEvent) {
+    const currentPage = this.gridApi.paginationGetCurrentPage();
+    //llamar servicio que cambia la páginaci
+    console.log(currentPage);
+    //this.gridApi.purgeVirtualPageCache
   }
 }
